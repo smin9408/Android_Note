93be3960669a3d7d994852bdca8235a024dc9b21
smin9408 <stmdaks888@naver.com>
2022-04-08 23:51:04 +0900

    var

--- app/src/main/java/com/example/androidnote/note/fastcampus_note.kt
+++ app/src/main/java/com/example/androidnote/note/fastcampus_note.kt
@@ -1,5 +1,11 @@
 package com.example.androidnote.note
 
-fun main(args:Array<String>){
+fun main() {
+    val int : Int
+    var num = 10
 
+    var a = 1 + 2 + 3 + 4 + 5 + 6
+    var b = "1"
+    var c = b.toInt()
+    var d = b.toFloat()
 }
