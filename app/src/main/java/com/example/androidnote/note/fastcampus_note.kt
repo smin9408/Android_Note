d53ff272cf5bcf32bf581a34911a0ceb5a74b35b
smin9408 <stmdaks888@naver.com>
2022-04-09 23:52:05 +0900

    Function

--- app/src/main/java/com/example/androidnote/note/fastcampus_note.kt
+++ app/src/main/java/com/example/androidnote/note/fastcampus_note.kt
@@ -1,11 +1,22 @@
 package com.example.androidnote.note
 
 fun main() {
-    val int : Int
+    val int: Int
     var num = 10
 
     var a = 1 + 2 + 3 + 4 + 5 + 6
     var b = "1"
     var c = b.toInt()
     var d = b.toFloat()
+
+    val sjf: Int? = null
+
+    println(name(5, 7))
+
+}
+
+
+fun name(first: Int, second: Int): Int {
+    val result = first + second
+    return result
 }
