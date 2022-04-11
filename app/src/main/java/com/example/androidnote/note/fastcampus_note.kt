b4fe3d8052f3dce69e85aa7e5c40b4d3d87c82be
smin9408 <stmdaks888@naver.com>
2022-04-11 23:59:03 +0900

    Today's study

--- app/src/main/java/com/example/androidnote/note/fastcampus_note.kt
+++ app/src/main/java/com/example/androidnote/note/fastcampus_note.kt
@@ -11,12 +11,45 @@ fun main() {
 
     val num2: Int? = null
 
-    println(name(5, 7))
 
+    plusMany(1,2,3,5,87,9,4,10)
+
+    val n2 = sum(1, 2, 3, 4, 5)
+    println(n2)
+
+    val test = arrayOf("A", "B")
+    showAll(*test)
 }
 
 
 fun name(first: Int, second: Int): Int {
     val result = first + second
     return result
+}
+
+// default 값을 갖는 함수
+fun plusFive(first: Int, second: Int = 5): Int {
+    val result: Int = first + second
+    return result
+}
+
+// 반환값이 없는 함수
+fun printPlus(first: Int, second: Int): Unit {
+    val result = first + second
+    println(result)
+}
+
+// 함수 축약
+fun shortFun(first: Int, second: Int):Int = first + second
+
+fun plusMany(vararg numbers: Int){
+    for (number in numbers){
+        println(number)
+    }
+}
+
+fun sum(vararg num: Int) = num.sum()
+
+fun showAll(vararg s: String) {
+    println(s.joinToString())
 }
