c82dea444f66b803bcc9062969acad15af70ad9e
smin9408 <stmdaks888@naver.com>
2022-04-12 23:43:26 +0900

    연산자

--- app/src/main/java/com/example/androidnote/note/fastcampus_note.kt
+++ app/src/main/java/com/example/androidnote/note/fastcampus_note.kt
@@ -1,55 +1,29 @@
 package com.example.androidnote.note
 
 fun main() {
-    val int: Int
-    var num = 10
 
-    var a = 1 + 2 + 3 + 4 + 5 + 6
-    var b = "1"
-    var c = b.toInt()
-    var d = b.toFloat()
-
-    val num2: Int? = null
-
-
-    plusMany(1,2,3,5,87,9,4,10)
-
-    val n2 = sum(1, 2, 3, 4, 5)
-    println(n2)
-
-    val test = arrayOf("A", "B")
-    showAll(*test)
 }
 
 
-fun name(first: Int, second: Int): Int {
-    val result = first + second
-    return result
-}
+// 연산자
 
-// default 값을 갖는 함수
-fun plusFive(first: Int, second: Int = 5): Int {
-    val result: Int = first + second
-    return result
-}
+// 산술 연산자
+// +, -, *, /(몫), %(나머지)
 
-// 반환값이 없는 함수
-fun printPlus(first: Int, second: Int): Unit {
-    val result = first + second
-    println(result)
-}
+// 대입 연산자
+// 좌변 = 우변 (우변 값이 조변에 들어간다)
 
-// 함수 축약
-fun shortFun(first: Int, second: Int):Int = first + second
+// 복합 대입 연산자
+// +=, -=, *=, /=, %=
+// a += 10 -> a = a + 10
 
-fun plusMany(vararg numbers: Int){
-    for (number in numbers){
-        println(number)
-    }
-}
+// 증감 연상자
+// ++, --
+// a++, a--
 
-fun sum(vararg num: Int) = num.sum()
+// 비교 연산자
+// >, <=, ==, !=
 
-fun showAll(vararg s: String) {
-    println(s.joinToString())
-}
+// 논리 연산자
+// &&, ||, !
+
