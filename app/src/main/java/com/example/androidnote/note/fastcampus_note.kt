d082a06ea24c674fe9202ddc524c0cb7509e9489
smin9408 <stmdaks888@naver.com>
2022-04-13 22:23:25 +0900

    배열

--- app/src/main/java/com/example/androidnote/note/fastcampus_note.kt
+++ app/src/main/java/com/example/androidnote/note/fastcampus_note.kt
@@ -2,34 +2,33 @@ package com.example.androidnote.note
 
 fun main() {
 
-    val a = 5
-    val b = 10
-    val value = 5
+    // 배열
 
-    // 조건문 when
-    when (value) {
-        1 -> {
-            println("value is 1")
-        }
-        2 -> {
-            println("value is 2")
-        }
-        3 -> {
-            println("value is 3")
-        }
-        else -> {
-            println("value is ${value}")
-        }
-    }
+    // 배열 생성 방법(1)
+    var arr1 = arrayOf<Int>(1, 2, 3, 4, 5, 6)
 
-    // 축약약
-   when (value) {
-        1 -> println("value is 1")
-        2 -> println("value is 2")
-        3 -> println("value is 3")
-        else -> println("value is ${value}")
-    }
+    // 배열 생성 방법(2)
+    val arr2 = arrayOf("joro", "usop", 3, 4, 6)
 
+    // 배열의 값을 꺼내는 방법(1)
+    val test1 = arr1.get(3)
+    println(test1)
+
+    // 배열의 값을 꺼내는 방법(2)
+    val test2 = arr2[1]
+    println(test2)
+
+    var arr3 = arrayOf(arr1, arr2)
+    val test3 = arr3.get(1).get(0)
+    println(test3)
+
+    // 배열의 값을 바꾸는 방법(1)
+    arr1.set(0, 100)
+    println(arr1[0])
+
+    // 배열의 값을 바꾸는 방법(2)
+    arr1[1] = 200
+    println(arr1[1])
 
 }
 
