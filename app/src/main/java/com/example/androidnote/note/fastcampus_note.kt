3f1a3e53ec0c552e5abb91f047d3ee0f0e163a90
smin9408 <stmdaks888@naver.com>
2022-04-14 23:57:27 +0900

    Collection  - list, set, map

--- app/src/main/java/com/example/androidnote/note/fastcampus_note.kt
+++ app/src/main/java/com/example/androidnote/note/fastcampus_note.kt
@@ -4,64 +4,58 @@ import java.util.*
 
 fun main() {
 
-    // 배열
+    // collection
+    // list, set, map
+    // Immutable Collection - 변경 불가
 
-    // 배열 생성 방법(1)
-    var arr1 = arrayOf<Int>(1, 2, 3, 4, 5, 6)
-
-    // 배열 생성 방법(2)
-    val arr2 = arrayOf("joro", "usop", 3, 4, 6, true, "hello")
-
-    // 배열의 값을 꺼내는 방법(1)
-    val test1 = arr1.get(3)
-    println(test1)
-
-    // 배열의 값을 꺼내는 방법(2)
-    val test2 = arr2[1]
-    println(test2)
-
-    var arr3 = arrayOf(arr1, arr2)
-    val test3 = arr3.get(1).get(0)
-    println(test3)
-
-    // 배열의 값을 바꾸는 방법(1)
-    arr1.set(0, 100)
-    println(arr1[0])
-
-    arr2[6] = 2
-    val pl = arr2.plus("추가")
-    println()
-    println(Arrays.toString(arr2))
-    println(arr2[0])
-    println(arr2[1])
-    println(arr2[2])
-    println(arr2[3])
-    println(arr2[4])
-    println(arr2[5])
-    println(arr2[6])
-    println()
-
-    // 배열의 값을 바꾸는 방법(2)
-    arr1[1] = 200
-    println(arr1[1])
+    // List
+    val numberList = listOf<Int>(1, 2, 3, 3)
+    println(numberList)
+    println(numberList[1])
+    println(numberList.get(2))
 
 
-    // 배열 생성 방법(3)
-    val a1 = intArrayOf(1, 2, 3) // int만 넣을 수 있는 배열
-    val a2 = charArrayOf('A', 'B') // char -> '' , string -> ""
-    val a3 = doubleArrayOf(1.5, 2.0, 2.4548)
-    val a4 = booleanArrayOf(true, false, true)
+    // set
+    // 중복을 허용하지 않음
+    // 순서가 없다 - index가 없다.
+    //
+    val numberSet = setOf(1, 2, 3, 3, 3, "sds")
+    numberSet.forEach {
+        println(it)
+    }
 
-    // 배열 생성 방법(4)
-    // lambda를 이용한 배열 생성
-    // 첫번째 인자는 size, 두번쨰는 들어갈 값  {  }부분을 람다라고 함
-    var a5 = Array(10, { 0 }) // size가 10 -> index가 0 ~ 9, 0 으로 이루어진 배열
 
-    var a6 = Array(5, { i -> i * 5 })
-    a6.set(4,100)
-    val a7 = a6[4]
-    println(a7)
+    // map
+    // key, value 방식
+    val numberMap = mapOf<String, Int>("one" to 1, "two" to 2, "three" to 3)
+    val numberMap2 = mapOf("one" to 1, 2 to 2, 3.0 to 3)
+    println(numberMap)
+    println(numberMap2)
+    println(numberMap.get("one"))
+    println(numberMap["two"])
+    println(numberMap2[3.0])
+
+
+    // Mutable Collection - 변경 가능
+    val mNumberList = mutableListOf<Int>(1, 2, 3, 4, 5)
+    mNumberList.add(5, 6) // 5번째 인덱스에 6 추가
+    mNumberList.add(3, 60) // 5번째 인덱스에 6 추가
+    mNumberList.add(6, 100) // 5번째 인덱스에 6 추가
+    println(mNumberList)
+
+
+    val mNumberSet = mutableSetOf<Int>(1, 2, 3, 4, 4, 4, 4)
+    mNumberSet.add(50)
+    println(mNumberSet)
+
+
+    val mNumberMap = mutableMapOf<String, Int>("one" to 1)
+    mNumberMap.put("two", 2)
+    mNumberMap.put("three", 3)
+    println(mNumberMap)
+
 
 }
+
 
 
