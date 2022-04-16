445532d53b2cf22d742be87bdf3268aa4033e935
smin9408 <stmdaks888@naver.com>
2022-04-16 23:51:16 +0900

    반복문 - 1

--- app/src/main/java/com/example/androidnote/note/fastcampus_note.kt
+++ app/src/main/java/com/example/androidnote/note/fastcampus_note.kt
@@ -2,59 +2,23 @@ package com.example.androidnote.note
 
 import java.util.*
 
+
+// 반복문
 fun main() {
 
-    // collection
-    // list, set, map
-    // Immutable Collection - 변경 불가
+    val a = mutableListOf<Int>(1, 2, 3, 4, 5, 6, 7, 66, 8, 9)
 
-    // List
-    val numberList = listOf<Int>(1, 2, 3, 3)
-    println(numberList)
-    println(numberList[1])
-    println(numberList.get(2))
-
-
-    // set
-    // 중복을 허용하지 않음
-    // 순서가 없다 - index가 없다.
-    //
-    val numberSet = setOf(1, 2, 3, 3, 3, "sds")
-    numberSet.forEach {
-        println(it)
+    // 반복하는 방법(1)
+    for (item in a) {
+        println(item)
     }
 
-
-    // map
-    // key, value 방식
-    val numberMap = mapOf<String, Int>("one" to 1, "two" to 2, "three" to 3)
-    val numberMap2 = mapOf("one" to 1, 2 to 2, 3.0 to 3)
-    println(numberMap)
-    println(numberMap2)
-    println(numberMap.get("one"))
-    println(numberMap["two"])
-    println(numberMap2[3.0])
-
-
-    // Mutable Collection - 변경 가능
-    val mNumberList = mutableListOf<Int>(1, 2, 3, 4, 5)
-    mNumberList.add(5, 6) // 5번째 인덱스에 6 추가
-    mNumberList.add(3, 60) // 5번째 인덱스에 6 추가
-    mNumberList.add(6, 100) // 5번째 인덱스에 6 추가
-    mNumberList.add(50) // 5번째 인덱스에 6 추가
-    println(mNumberList)
-
-
-    val mNumberSet = mutableSetOf<Int>(1, 2, 3, 4, 4, 4, 4)
-    mNumberSet.add(50)
-    println(mNumberSet)
-
-
-    val mNumberMap = mutableMapOf<String, Int>("one" to 1)
-    mNumberMap.put("two", 2)
-    mNumberMap.put("three", 3)
-    mNumberMap.replace("one",100)
-    println(mNumberMap)
+    // 반복하는 방법(2)
+    // index, item 의 이름은 마음대로 정해도 가능
+    // 첫번째 인자는 index, 두번째는 item
+    for( (index, item) in a.withIndex()){
+        println("index : ${index} value : ${item}")
+    }
 
 
 }
