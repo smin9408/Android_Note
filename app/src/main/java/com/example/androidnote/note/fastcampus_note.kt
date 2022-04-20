821af8ba1791d8c7f3c469b9dffeb1c3e09b5690
smin9408 <stmdaks888@naver.com>
2022-04-20 23:58:38 +0900

    Class - 3

--- app/src/main/java/com/example/androidnote/note/fastcampus_note.kt
+++ app/src/main/java/com/example/androidnote/note/fastcampus_note.kt
@@ -1,7 +1,6 @@
 package com.example.androidnote.note
 
 
-// 반복문
 fun main() {
 
     // Class
@@ -14,6 +13,18 @@ fun main() {
     val myCar: Car = Car("v8 engine", "big")
 
     val superCar: SuperCar = SuperCar("fast engine", "red", "2door")
+
+    // 인스턴스가 가지고 있는 기능을 사용하는 방법
+    val driveCar: DriveCar = DriveCar("engine", "body")
+    driveCar.drive()
+    driveCar.navi("우리 집")
+    driveCar.ride()
+
+    // 인스턴스의 멤버 변수에 접근 하는 방법
+    val driveCar2: DriveCar2 = DriveCar2("2engine", "2body")
+    println(driveCar2.body)
+    println(driveCar2.engine)
+
 }
 
 // 클래스 만드는 방법(1)
@@ -46,21 +57,64 @@ class Car2(engine: String, body: String) {
 }
 
 // 클래스 만드는 방법(4)
-class Car3{
+class Car3 {
     var engine: String = ""
     var body: String = ""
     var door: String = ""
 
-    constructor(engine: String, body: String){
+    constructor(engine: String, body: String) {
         this.engine = engine
         this.body = body
     }
 
-    constructor(engine: String, body: String, door: String){
+    constructor(engine: String, body: String, door: String) {
         this.engine = engine
         this.body = body
         this.door = door
     }
 
 
+}
+
+class DriveCar(engine: String, body: String) {
+    fun ride() {
+        println("탑승 하였습니다")
+    }
+
+    fun drive() {
+        println("달립니다")
+    }
+
+    fun navi(destination: String) {
+        println("${destination}로 목적지가 설정되었습니다")
+    }
+}
+
+class DriveCar2 {
+    var engine: String
+    var body: String
+
+    constructor(engine: String, body: String) {
+        this.engine = engine
+        this.body = body
+    }
+
+    // 인스턴스화 될 때 가장 먼저 호출된다.
+    // 초기값을 넣어 줄 때 유용하다.
+    init {
+        println("Car2가 만들어 졌습니다.")
+        drive()
+    }
+
+    fun ride() {
+        println("탑승 하였습니다")
+    }
+
+    fun drive() {
+        println("달립니다")
+    }
+
+    fun navi(destination: String) {
+        println("${destination}로 목적지가 설정되었습니다")
+    }
 }
