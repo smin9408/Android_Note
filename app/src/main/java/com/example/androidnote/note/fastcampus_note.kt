bb589ae154062a94aa8421ba338848b3c58819df
smin9408 <stmdaks888@naver.com>
2022-04-29 10:35:41 +0900

    Calculator

--- app/src/main/java/com/example/androidnote/note/fastcampus_note.kt
+++ app/src/main/java/com/example/androidnote/note/fastcampus_note.kt
@@ -1,36 +1,39 @@
 package com.example.androidnote.note
 
-
 fun main() {
 
-    val myCard: Card = Card("이승민", "940803", 500000)
-    myCard.save(500000)
-    myCard.check()
-    myCard.out(250000)
-    myCard.check()
+    val CalculatorS = Calculator(10)
+    CalculatorS.plus(20).minus(50).plus(600).multiply(2).answer().divide(10).answer()
 }
 
-class Card(val name: String, val birth: String, var money: Int) {
-    fun create() {
-        println("${birth} 년생 ${name}님의 계좌가 생성되었습니다.")
+// Chaining(체이닝)
+// 자기 자신 클래스를 return 해서 기능을 이어 나가는 것
+
+class Calculator(val initialValue: Int) {
+
+    fun plus(number: Int): Calculator {
+        val result = this.initialValue + number
+        return Calculator(result)
     }
 
-    fun check() {
-        println("게좌의 잔고는 ${money}원 입니다.")
+    fun minus(number: Int): Calculator {
+        val result = this.initialValue - number
+        return Calculator(result)
     }
 
-    fun out(outMoney: Int) {
-        money -= outMoney
+    fun multiply(number: Int): Calculator {
+        val result = this.initialValue * number
+        return Calculator(result)
     }
 
-    fun save(saveMoney: Int) {
-        money += saveMoney
-        val hue:String
-        val hue2:String
-        val todat:TurningPoint
+    fun divide(number:Int): Calculator{
+        val result = this.initialValue / number
+        return Calculator(result)
+    }
 
-
+    fun answer(): Calculator{
+        println(this.initialValue)
+        return Calculator(this.initialValue)
     }
 
 }
-class TurningPoint(){}
