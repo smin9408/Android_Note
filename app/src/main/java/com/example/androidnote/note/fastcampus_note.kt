17b64b567329b3e433cddacbf31c65e3e88c3e97
smin9408 <stmdaks888@naver.com>
2022-04-30 22:57:19 +0900

    상속

--- app/src/main/java/com/example/androidnote/note/fastcampus_note.kt
+++ app/src/main/java/com/example/androidnote/note/fastcampus_note.kt
@@ -1,39 +1,49 @@
 package com.example.androidnote.note
 
-fun main() {
+fun main(args: Array<String>) {
 
-    val CalculatorS = Calculator(10)
-    CalculatorS.plus(20).minus(50).plus(600).multiply(2).answer().divide(10).answer()
+    val bus = Bus("v6", "bus")
+
+    bus.drive()
+
+    val superCar = SuperCar()
+    superCar.drive()
 }
 
-// Chaining(체이닝)
-// 자기 자신 클래스를 return 해서 기능을 이어 나가는 것
+// class는 생성 시 private이기 때문에 open 키워드를 붙여야 상속이 가능하다.
+// 부모 class의 함수도 마찬가지로 open 키워드를 사용해야 자식 class에서 override 가능
+open abstract class Car() {
+    open fun drive() {
 
-class Calculator(val initialValue: Int) {
-
-    fun plus(number: Int): Calculator {
-        val result = this.initialValue + number
-        return Calculator(result)
     }
 
-    fun minus(number: Int): Calculator {
-        val result = this.initialValue - number
-        return Calculator(result)
+    fun stop() {
+
     }
 
-    fun multiply(number: Int): Calculator {
-        val result = this.initialValue * number
-        return Calculator(result)
+    abstract fun new()
+}
+
+class SuperCar() : Car() {
+    override fun drive(){
+        println("더 빨리!")
     }
 
-    fun divide(number:Int): Calculator{
-        val result = this.initialValue / number
-        return Calculator(result)
+    override fun new() {
+    }
+}
+
+class Electronic(engine: String, body: String) : Car() {
+    override fun new() {
+
     }
 
-    fun answer(): Calculator{
-        println(this.initialValue)
-        return Calculator(this.initialValue)
+}
+
+class Bus(engine: String, body: String) : Car() {
+    override fun new() {
+
     }
+
 
 }
