523b6061c1ee2b1bd0c9a2258172df35bce841d8
smin9408 <stmdaks888@naver.com>
2022-05-01 23:33:42 +0900

    상속2

--- app/src/main/java/com/example/androidnote/note/fastcampus_note.kt
+++ app/src/main/java/com/example/androidnote/note/fastcampus_note.kt
@@ -12,7 +12,7 @@ fun main(args: Array<String>) {
 
 // class는 생성 시 private이기 때문에 open 키워드를 붙여야 상속이 가능하다.
 // 부모 class의 함수도 마찬가지로 open 키워드를 사용해야 자식 class에서 override 가능
-open abstract class Car() {
+abstract class Car() {
     open fun drive() {
 
     }
@@ -25,7 +25,7 @@ open abstract class Car() {
 }
 
 class SuperCar() : Car() {
-    override fun drive(){
+    override fun drive() {
         println("더 빨리!")
     }
 
