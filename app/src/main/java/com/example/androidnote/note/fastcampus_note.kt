02b2b914bb3b548ed788d9d7be6dda1c29def098
smin9408 <stmdaks888@naver.com>
2022-05-05 23:43:36 +0900

    자료구조 Stack

--- app/src/main/java/com/example/androidnote/note/fastcampus_note.kt
+++ app/src/main/java/com/example/androidnote/note/fastcampus_note.kt
@@ -7,24 +7,14 @@ import java.util.*
 // 타입을 체크하는 기능
 
 fun main(args:Array<String>){
-    var q : Queue<Int> = LinkedList() // 큐로 선언하고 LinkedList 로 할당
-    q.add(1)    // 객체를 큐에 추가 (큐가 가득찬 상태이면 illegalStateException 발생)
-    q.offer(3)  // 객체를 큐에 추가 (큐가 가득찬 상태이면 false 반환)
-    q.add(2)
-    println(q)
+    var s = Stack<Int>()
 
-    println()
+    s.push(1) // 객체를 스택에 추가
+    s.push(3)
 
-    println(q.element())    // 맨 앞 객체 리턴 (큐가 비어있는 상태이면 NoSuchElementException 발생)
-    println(q.elementAt(1)) // 인덱스 값의 객체 리턴
-    println(q.peek())   // 맨 앞 객체 리턴 (큐가 비어있는 상태이면 false 반환)
+    s.peek() // 맨 위의 객체 반환 (비어 있는 상태이면 EmptyStackException 발생)
+    println(s)
 
-    println()
-
-    q.remove() // 1을 삭제하고 반환  // 삭제하면서 객체 반환 (큐가 비어있는 상태이면 NoSuchElementException 발생)
-    println(q) // [3]
-    var tmp = q.poll()  // 삭제하면서 객체 반환 (큐가 비어있는 상태이면 false 반환)
-    println(q)
-    println("두번째 삭제한 객체 : $tmp")
-
+    s.pop() // 맨 위의 객체 삭제하고 반환 (비어 있는 상태이면 EmptyStackException 발생)
+    println(s)
 }
