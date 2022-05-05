b1c0d9a9ab907745a127185a4e3e65ece27dcc9a
smin9408 <stmdaks888@naver.com>
2022-05-05 23:44:06 +0900

    자료구조 PriorityQueue

--- app/src/main/java/com/example/androidnote/note/fastcampus_note.kt
+++ app/src/main/java/com/example/androidnote/note/fastcampus_note.kt
@@ -6,15 +6,17 @@ import java.util.*
 // Generic(제너릭)
 // 타입을 체크하는 기능
 
-fun main(args:Array<String>){
-    var s = Stack<Int>()
+fun main(args: Array<String>) = with(Scanner(System.`in`)){
+    val q = PriorityQueue<Int>() // 오름차순
 
-    s.push(1) // 객체를 스택에 추가
-    s.push(3)
+    val q2 = PriorityQueue<Int>(Collections.reverseOrder()) // 내림차순
 
-    s.peek() // 맨 위의 객체 반환 (비어 있는 상태이면 EmptyStackException 발생)
-    println(s)
+    q.addAll(listOf(1,5,2,4,3))
+    q2.addAll(listOf(1,5,2,4,3))
 
-    s.pop() // 맨 위의 객체 삭제하고 반환 (비어 있는 상태이면 EmptyStackException 발생)
-    println(s)
+    // 우선순위가 높은 것부터 반환
+    while(!q.isEmpty()) print("${q.poll()} ")
+    println()
+    while(!q2.isEmpty()) print("${q2.poll()} ")
+
 }
