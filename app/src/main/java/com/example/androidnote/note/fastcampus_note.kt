fa41ec7f8b6449fd538721840a0004572c2d9a9b
smin9408 <stmdaks888@naver.com>
2022-05-06 23:43:42 +0900

    BufferedReader

--- app/src/main/java/com/example/androidnote/note/fastcampus_note.kt
+++ app/src/main/java/com/example/androidnote/note/fastcampus_note.kt
@@ -1,22 +1,12 @@
 package com.example.androidnote.note
 
-import java.util.*
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
 
-// Generic(제너릭)
-// 타입을 체크하는 기능
+fun main(args: Array<String>) {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val n = br.readLine().toInt()
 
-fun main(args: Array<String>) = with(Scanner(System.`in`)){
-    val q = PriorityQueue<Int>() // 오름차순
-
-    val q2 = PriorityQueue<Int>(Collections.reverseOrder()) // 내림차순
-
-    q.addAll(listOf(1,5,2,4,3))
-    q2.addAll(listOf(1,5,2,4,3))
-
-    // 우선순위가 높은 것부터 반환
-    while(!q.isEmpty()) print("${q.poll()} ")
-    println()
-    while(!q2.isEmpty()) print("${q2.poll()} ")
-
+    println(n)
 }
