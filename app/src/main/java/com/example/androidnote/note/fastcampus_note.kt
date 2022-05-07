735d9ff06064f56b43af9f0fdcbbd62664ddd141
smin9408 <stmdaks888@naver.com>
2022-05-07 23:39:19 +0900

    코테 - 이메일 찾기

--- app/src/main/java/com/example/androidnote/note/fastcampus_note.kt
+++ app/src/main/java/com/example/androidnote/note/fastcampus_note.kt
@@ -1,12 +1,56 @@
 package com.example.androidnote.note
 
-import java.io.BufferedReader
-import java.io.InputStreamReader
+fun main() {
 
+    val emailList1 = arrayListOf<String>(
+        "jay@naver.com",
+        "john@naver.com",
+        "emily@google.com",
+        "ken@google.com",
+        "stmdals@kakao.com"
+    )
 
-fun main(args: Array<String>) {
-    val br = BufferedReader(InputStreamReader(System.`in`))
-    val n = br.readLine().toInt()
+    val result1 = solution(emailList1)
+    println(result1)
 
-    println(n)
+    val emailList2 = arrayListOf<String>(
+        "Aiden@naver.com",
+        "Andew@naver.com",
+        "Adrian@daum.net",
+        "Asher@google.com",
+        "Austin@kakao.com",
+        "Antoni@google.com"
+    )
+
+    val result2 = solution(emailList2)
+    println(result2)
+
+}
+
+fun solution(emailList: ArrayList<String>): Map<String, Int> {
+
+    var naverCount = 0
+    var googleCount = 0
+    var elseCount = 0
+
+    for (email in emailList) {
+        val domain = email.split("@")[1]
+        val emailItem = domain.split(".")[0]
+
+        if (emailItem == "naver") {
+            naverCount += 1
+        } else if (emailItem == "google") {
+            googleCount += 1
+        } else {
+            elseCount += 1
+        }
+    }
+
+    val resultMap = mutableMapOf<String, Int>(
+        "naver" to naverCount,
+        "google" to googleCount,
+        "else" to elseCount
+    )
+
+    return resultMap
 }
