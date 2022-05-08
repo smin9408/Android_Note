9413fb0b981eb6d14028518a0ff766cf494e4a9e
smin9408 <stmdaks888@naver.com>
2022-05-08 23:08:10 +0900

    코테 - 문자열의 숫자 및 홀짝 구분

---
+++ app/src/main/java/com/example/androidnote/note/coding_test_note.kt
@@ -0,0 +1,42 @@
+package com.example.androidnote.note
+
+// 문자열의 숫자와 짝수(true)와 홀수(false)를 알려주는 solution을 만드세요
+
+fun main(){
+
+    val result1 = solution("abcd")
+    println(result1)
+    // [4, true]
+
+    val result2 = solution("abcde")
+    println(result2)
+    // [5, false]
+}
+
+
+fun solution(str : String) : ArrayList<String> {
+
+    // 길이 구하는 것 하나랑
+    val length = str.length
+    //println(length)
+
+    // 구해진 길이가 홀수인지 짝수인지 알려주는 기능
+
+    /*
+    var isEven = true
+    if(length % 2 == 0){
+        //println("짝수")
+        isEven = true
+    } else {
+        //println("홀수")
+        isEven = false
+    }
+    */
+
+    val isEven = length % 2 == 0
+
+    val resultArray = arrayListOf<String>(length.toString(), isEven.toString())
+
+    return resultArray
+
+}
