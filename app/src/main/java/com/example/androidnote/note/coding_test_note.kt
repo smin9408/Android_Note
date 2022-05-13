5b65dc213ff000dc6bfab1ead7fc85a8c25f7ed1
smin9408 <stmdaks888@naver.com>
2022-05-13 23:26:13 +0900

    코테 - 알파벳 숫자 세기

--- app/src/main/java/com/example/androidnote/note/coding_test_note.kt
+++ app/src/main/java/com/example/androidnote/note/coding_test_note.kt
@@ -1,48 +1,62 @@
 package com.example.androidnote.note
 
-// 숫자를 받아서 팩토리얼 값을 출력하는 함수를 만드세요
-// 3! -> 3 x 2 x 1 = 6
-// 4! -> 4 x 3 x 2 x 1 = 24
-// 5! -> 5 x 4 x 3 x 2 x 1 = 120
+// 문자의 갯수를 세는 solution을 만들어주세요
+// 문자열이 많이 나온 순서대로 정렬됩니다.
+// 문자열 갯수가 같은 경우는 없다고 가정합니다. (a 는 5개 b 는 5개 이런 경우는 없습니다.)
 
 fun main(){
 
-    val result1 = solution(3)
+    val result1 = solution("aabbaabccbb")
     println(result1)
-    // 6
+    // {b=5, a=4, c=2}
 
-    val result2 = solution(4)
+    val result2 = solution("ccddcddaaq")
     println(result2)
-    // 24
-
-    val result3 = solution(5)
-    println(result3)
-    // 120
+    // {d=4, c=3, a=2, q=1}
 
 }
 
 
-fun solution( number : Int ) : Int {
+fun solution( str : String ): Map<String, Int> {
 
-    // 3 x 2 x 1 -> 6
-    // 1 x 2 x 3 -> 6
+    // 일단은 문자열에 어떤 알파벳이 있는지 찾기
 
-    var result = 1
+    val resultArr = mutableListOf<String>()
 
-    for (i in 1..number){
-        //println(i)
-        result *= i // result = result * i
-        // i = 1 -> result -> 1 * 1 = 1
-        // i = 2 -> result -> 1 * 2 = 2
-        // i = 3 -> result -> 2 * 3 = 6
-        // i = 4 -> result -> 6 * 4 = 24
-        // i = 5 -> result -> 24 * 5 = 120
+    for(i in str) {
+        resultArr.add(i.toString())
     }
 
-    return result
+    //println(resultArr)
+    //println(resultArr.distinct())
+
+    val distinctList = resultArr.distinct()
+    val countList =  mutableListOf<Int>()
+    // 어떤 알파벳이 몇번 사용되었는지 세기
+
+
+    for (i in distinctList){ // a b c
+        val count = resultArr.filter { it == i}.count()
+        // println(count)
+        countList.add(count)
+    }
+
+
+    // 알파벳이 많이 나온 순서대로 정렬
+
+    val resultMap = mutableMapOf<String, Int>()
+
+    for (i in 0..distinctList.count() - 1){
+        //println(distinctList[i])
+        //println(countList[i])
+        resultMap[distinctList[i]] = countList[i]
+    }
+
+    // println(resultMap.toList().sortedBy {it.second}.reversed().toMap() )
+
+    return resultMap.toList().sortedBy {it.second}.reversed().toMap()
 
 }
-
 
 
 
