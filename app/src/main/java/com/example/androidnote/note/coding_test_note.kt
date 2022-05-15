4549cbf238ba1d40853ad483284134e5095d569f
smin9408 <stmdaks888@naver.com>
2022-05-15 23:53:18 +0900

    코테 - 가까운 사람 이름 찾기

--- app/src/main/java/com/example/androidnote/note/coding_test_note.kt
+++ app/src/main/java/com/example/androidnote/note/coding_test_note.kt
@@ -1,70 +1,52 @@
 package com.example.androidnote.note
 
-// 리스트의 가장 작은 숫자를 찾아서 제거하고
-// 남은 숫자들을 모두 더해서 값을 출력해주는 함수를 만드세요
+// 가장 가까운 사람의 이름을 알려주는 함수를 완성하세요
+// 거리는 음수가 아닙니다 / 같은 거리는 없습니다.
 
 fun main(){
 
-    val input1 = arrayListOf(1,2,3,4,5)
-    val result1 = solution(input1)
+    val result1 = solution("민수 : 3, 민지  : 4, 찬혁 : 1.5, 지우 : 2")
     println(result1)
-    // 14
+    // 찬혁
 
-    val input2 = arrayListOf(10,8,6,4,2)
-    val result2 = solution(input2)
+    val result2 = solution("지영 : 3.7, 민영  : 1.2 , 해피 : 2.7, 퍼니 : 3")
     println(result2)
-    // 28
-
-    val input3 = arrayListOf(6, 3, 9)
-    val result3 = solution(input3)
-    println(result3)
-    // 15
+    // 민영
 
 }
 
 
-fun solution(list: ArrayList<Int>) : Int {
+fun solution(str: String) : String {
 
-    /*
-    val min = list.minOrNull()
-    println(min)
+    val list = str.split(",")
+    //println(list)
 
-    list.remove(min)
+    val nameList = mutableListOf<String>()
+    val distanceList = mutableListOf<Double>()
 
-    println(list)
+    for (item in list) {
 
-    var result = 0
+        val name = item.split(":")[0]
+        val distance = item.split(":")[1].toDouble()
 
-    for (i in list) {
-        result += i
-    }
-    */
+        nameList.add(name)
+        distanceList.add(distance)
 
-    var smallest = list[0]
-
-    for (i in 1..list.size-1){
-        // println(list[i])
-        if(smallest > list[i]){
-            smallest = list[i]
-        }
     }
 
-    list.remove(smallest)
+    //println(nameList)
+    //println(distanceList)
 
-    var result = 0
+    val minDistance = distanceList.minOrNull()
 
-    for (i in list) {
-        result += i
-    }
+    //println(minDistance)
 
-    return result
+    val resultIndex = distanceList.indexOf(minDistance)
+
+    //println(resultIndex)
+
+
+
+    return nameList[resultIndex].trim()
 }
-
-
-
-
-
-
-
-
 
